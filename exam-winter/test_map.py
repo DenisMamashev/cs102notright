01dde64879dff28b0486b5c2f80a65fb277ad16c
DenisMamashev <71326386+DenisMamashev@users.noreply.github.com>
2021-01-23 15:58:38 +0600

    exam/done

---
+++ exam-winter/test_map.py
@@ -0,0 +1,13 @@
+import unittest
+
+import map.py
+
+
+class TestCase(unittest.TestCase):    
+    
+    def test_solve(self):
+        grid = [
+            
+        ]
+
+        self.assertFalse(map.check_solution(bad_solution))
